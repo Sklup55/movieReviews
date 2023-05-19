5e4a2f12b4e2b3f5c68721babf7029ab12e0acd1
Sklup55 <85282385+Sklup55@users.noreply.github.com>
2023-05-19 15:28:59 +0530

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,36 +5,36 @@ let isValid = require("./registered.js").isValid;
 let users = require("./registered.js").users;
 const axios = require("axios").default;
 
-
+//Logging in as a registered user (Task 6)
 public_users.post("/register", (req,res) => {
   // Complete the code for registering a new user 
   return res.status(404).json({message: "Unable to register user!"});
 });
 
-// Get the list of movies
+// Get the list of movies (Task 1)
 public_users.get('/',function (req, res) {
 //   Complete the code for getting the list of movies available in the shop
 });
 
-// Get movies based on ID
+// Get movies based on ID  (Task 2)
 public_users.get('/id/:id',function (req, res) {
 //   Complete the code for getting the movie details based on id
   res.send(movies[id])
  });
   
-// Get movies based on director
+// Get movies based on director  (Task 3)
 public_users.get('/director/:director',function (req, res) {
 // Complete the code for getting the movie details based on the director
   res.send(JSON.stringify({moviesbydirector}, null, 4));
 });
 
-// Get movies by their name
+// Get movies by their name   (Task 4)
 public_users.get('/name/:name',function (req, res) {
 // Complete the code for getting the movie details based on the name
   res.send(JSON.stringify({moviesbyname}, null, 4));
 });
 
-//  Get movie reviews by ID
+//  Get movie reviews by ID    (Task 5)
 public_users.get('/review/:id',function (req, res) {
   // Complete the code for getting movie reviews based on id
 });
